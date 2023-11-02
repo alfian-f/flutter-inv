bc497668f723660374e94bb030356a50cb3ca646
alfian-f <alfian.0615@gmail.com>
2023-11-02 10:25:28 +0700

    Assignment 7

---
+++ README.md
@@ -0,0 +1,38 @@
+# flutter_inv
+
+Flutter Assignment for PBP Course.
+
+## Assignment 7
+-   What are the main differences between stateless and stateful widget in Flutter?
+In stateless widget, they do not change by themselves through some internal action or behavior. They are changed by external events on parent widgets in the widgets tree. While stateful widget change their descriptions dynamically during their lifetimes.
+---
+-   Explain all widgets that you used in this assignment.
+
+---
+-   Explain how you implemented the checklist above step-by-step (not just following the tutorial).
+
+---
+
+## Getting Started
+
+  
+
+This project is a starting point for a Flutter application.
+
+  
+
+A few resources to get you started if this is your first Flutter project:
+
+  
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+  
+
+For help getting started with Flutter development, view the
+
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/menu.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  final List<InvItem> items = [
+    InvItem("View Items", Icons.checklist),
+    InvItem("Add Item", Icons.add_shopping_cart),
+    InvItem("Logout", Icons.logout),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Item List',
+        ),
+      ),
+      body: SingleChildScrollView(
+        // Scrolling wrapper widget
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding for the page
+          child: Column(
+            // Widget to display children vertically
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Text widget to display text with center alignment and appropriate style
+                child: Text(
+                  'Flutter Inventory', // Text indicating the app name
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container for our cards.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((InvItem item) {
+                  // Iteration for each item
+                  return InvCard(item);
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InvItem {
+  final String name;
+  final IconData icon;
+
+  InvItem(this.name, this.icon);
+}
+
+class InvCard extends StatelessWidget {
+  final InvItem item;
+
+  const InvCard(this.item, {Key? key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.indigo,
+      child: InkWell(
+        // Responsive touch area
+        onTap: () {
+          // Show a SnackBar when clicked
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("You clicked the ${item.name} button!")));
+        },
+        child: Container(
+          // Container to hold Icon and Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
